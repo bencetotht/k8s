b80347b249b3e56d0916fd34ca9f41bd3f1063ef
BenceToth <tothbencetamas@gmail.com>
2022-12-10 16:32:23 +0100

    lot

---
+++ terraform/docker.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    docker = {
+        source: "kreuzwerker/docker"
+        version: "2.23.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+    name = "nginx:latest"
+}
+
+resource "docker_container" "nginx_container" {   
+    name = "random_nginx_container"
+    image = docker_image.nginx.image_id
+    ports {
+        internal = 8000
+        external = 8000
+    }
+}
